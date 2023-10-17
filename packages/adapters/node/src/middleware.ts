4814dd060b4011d075301cde68d13e9ecc31cbba
Ryan Turnquist <rturnq@gmail.com>
2023-10-17 14:11:45 -0700

    feat: cache node middlewares

--- packages/adapters/node/src/middleware.ts
+++ packages/adapters/node/src/middleware.ts
@@ -21,41 +21,47 @@ export interface MatchedRoute {
 
 type MatchedRequest = IncomingMessage & { route: MatchedRoute };
 
+let _routerMiddleware: NodeMiddleware | undefined;
 export const routerMiddleware = ensureRuntime(() => {
-  return createMiddleware((request, platform) =>
+  return (_routerMiddleware ??= createMiddleware((request, platform) =>
     globalThis.__marko_run__.fetch(request, platform)
-  );
+  ));
 });
 
+let _invokeMiddlware: NodeMiddleware | undefined;
 export const invokeMiddleware = ensureRuntime(() => {
-  return createMiddleware((request, platform) =>
+  return (_invokeMiddlware ??= createMiddleware((request, platform) =>
     globalThis.__marko_run__.invoke(
       (platform.request as MatchedRequest).route.match,
       request,
       platform
     )
-  );
+  ));
 });
 
+let _matchMiddleware: NodeMiddleware | undefined;
 export const matchMiddleware = ensureRuntime(() => {
-  const invoke = invokeMiddleware();
-  return (req, _res, next) => {
-    const { url, method } = req as { url: string; method: string };
-    const queryIndex = url.indexOf("?");
-    const pathname = queryIndex === -1 ? url : url.slice(0, queryIndex);
-    const match = globalThis.__marko_run__.match(method, pathname);
-    if (match) {
-      (req as MatchedRequest).route = {
-        invoke,
-        match,
-        config: {
-          ...(match.meta as any),
-          _handler: invoke,
-        },
-      };
-    }
-    next?.();
-  };
+  if (!_matchMiddleware) {
+    const invoke = invokeMiddleware();
+    _matchMiddleware = (req, _res, next) => {
+      const { url, method } = req as { url: string; method: string };
+      const queryIndex = url.indexOf("?");
+      const pathname = queryIndex === -1 ? url : url.slice(0, queryIndex);
+      const match = globalThis.__marko_run__.match(method, pathname);
+      if (match) {
+        (req as MatchedRequest).route = {
+          invoke,
+          match,
+          config: {
+            ...(match.meta as any),
+            _handler: invoke,
+          },
+        };
+      }
+      next?.();
+    };
+  }
+  return _matchMiddleware;
 });
 
 export function importRouterMiddleware(): NodeMiddleware {
