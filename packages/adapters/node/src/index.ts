9de2ed728df89874901dde74064b298ac192d8ad
Ryan Turnquist <rturnq@gmail.com>
2024-03-07 16:59:33 -0800

    fix: no-external node adapter imports

--- packages/adapters/node/src/index.ts
+++ packages/adapters/node/src/index.ts
@@ -1,10 +1,21 @@
+import { mergeConfig } from "vite";
 import baseAdapter, { type Adapter } from "@marko/run/adapter";
 export type { NodePlatformInfo } from "@marko/run/adapter";
 
 export default function (): Adapter {
+  const base = baseAdapter();
   return {
-    ...baseAdapter(),
+    ...base,
     name: "node-adapter",
+    async viteConfig(config) {
+      const baseConfig = await base.viteConfig?.(config);
+      const adapterConfig = {
+        ssr: {
+          noExternal: /@marko\/run-adapter-node/
+        }
+      }
+      return baseConfig ? mergeConfig(baseConfig, adapterConfig) : adapterConfig;
+    },
     typeInfo(writer) {
       writer(`import type { NodePlatformInfo } from '@marko/run-adapter-node'`);
       return "NodePlatformInfo";
