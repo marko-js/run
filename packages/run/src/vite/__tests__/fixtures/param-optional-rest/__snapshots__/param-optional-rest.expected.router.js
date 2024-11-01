665eec1bf8e7a991af67c880f55990695222f44a
Ryan Turnquist <rturnq@gmail.com>
2024-11-01 10:47:51 -0700

    fix: known param missing when followed by rest param

---
+++ packages/run/src/vite/__tests__/fixtures/param-optional-rest/__snapshots__/param-optional-rest.expected.router.js
@@ -0,0 +1,68 @@
+// @marko/run/router
+import { NotHandled, NotMatched, createContext } from 'virtual:marko-run/runtime/internal';
+import { get1 } from 'virtual:marko-run/__marko-run__route.$campaignId.js';
+import { get2 } from 'virtual:marko-run/__marko-run__route.$campaignId.$$rest.js';
+
+globalThis.__marko_run__ = { match, fetch, invoke };
+    
+export function match(method, pathname) {
+	if (!pathname) {
+    pathname = '/';
+  } else if (pathname.charAt(0) !== '/') {
+    pathname = '/' + pathname;
+  }
+	switch (method) {
+		case 'GET':
+		case 'get': {
+			const len = pathname.length;
+			if (len > 1) {
+				const i1 = pathname.indexOf('/', 1) + 1;
+				if (!i1 || i1 === len) {
+					const s1 = decodeURIComponent(pathname.slice(1, i1 ? -1 : len));
+					if (s1) return { handler: get1, params: { campaignId: s1 }, meta: {}, path: '/:campaignId' }; // /$campaignId
+				} else {
+					const s1 = decodeURIComponent(pathname.slice(1, i1 - 1));
+					if (s1) {
+						return { handler: get2, params: { campaignId: s1, rest: pathname.slice(i1) }, meta: {}, path: '/:campaignId/:rest*' }; // /$campaignId/$$rest
+					}
+				}
+			}
+			return null;
+		}
+	}
+	return null;
+}
+
+export async function invoke(route, request, platform, url) {
+	const [context, buildInput] = createContext(route, request, platform, url);
+	if (route) {
+		try {
+			const response = await route.handler(context, buildInput);
+			if (response) return response;
+		} catch (error) {
+			if (error === NotHandled) return;
+			if (error !== NotMatched) throw error;
+		}
+	}
+}
+
+export async function fetch(request, platform) {
+  try {
+    const url = new URL(request.url);
+    let { pathname } = url;
+    if (pathname !== '/' && pathname.endsWith('/')) {
+      url.pathname = pathname.slice(0, -1);
+      return Response.redirect(url);
+    }   
+
+    const route = match(request.method, pathname);
+    return await invoke(route, request, platform, url);
+  } catch (error) {
+    if (import.meta.env.DEV) {
+      throw error;
+    }
+    return new Response(null, {
+      status: 500
+    });
+  }
+}
---
+++ packages/run/src/vite/__tests__/fixtures/param-optional-rest/__snapshots__/param-optional-rest.expected.routes.md
@@ -0,0 +1,43 @@
+# Routes
+
+## Route `/$campaignId`
+### Paths
+  - `/$campaignId`
+### Template
+```marko
+// virtual:marko-run/__marko-run__route.$campaignId.marko
+import page from './src/routes/$campaignId/$$rest,/+page.marko';
+
+<page ...input/>
+```
+### Handler
+```js
+// virtual:marko-run/__marko-run__route.$campaignId.js
+import { pageResponse } from 'virtual:marko-run/runtime/internal';
+import page from 'virtual:marko-run/__marko-run__route.$campaignId.marko?marko-server-entry';
+
+export async function get1(context, buildInput) {
+	return pageResponse(page, buildInput());
+}
+```
+---
+## Route `/$campaignId/$$rest`
+### Paths
+  - `/$campaignId/$$rest`
+### Template
+```marko
+// virtual:marko-run/__marko-run__route.$campaignId.$$rest.marko
+import page from './src/routes/$campaignId/$$rest,/+page.marko';
+
+<page ...input/>
+```
+### Handler
+```js
+// virtual:marko-run/__marko-run__route.$campaignId.$$rest.js
+import { pageResponse } from 'virtual:marko-run/runtime/internal';
+import page from 'virtual:marko-run/__marko-run__route.$campaignId.$$rest.marko?marko-server-entry';
+
+export async function get2(context, buildInput) {
+	return pageResponse(page, buildInput());
+}
+```
