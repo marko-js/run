ff6d1a080c53719642158512238ce652e3fcd3e5
Ryan Turnquist <rturnq@gmail.com>
2025-04-04 10:26:38 -0700

    fix: disable timeout for netlify test

--- packages/run/src/__tests__/fixtures/netlify-adapter-page/test.config.ts
+++ packages/run/src/__tests__/fixtures/netlify-adapter-page/test.config.ts
@@ -1,5 +1,6 @@
 export const steps = [click, click, click];
 export const preview_args = ["--offline"];
+export const skip_preview = process.env.CI === "true";
 
 async function click() {
   await page.click("button");
